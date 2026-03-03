6ffebd7f56835f113715eab70d78edbaecc1f675
JP <papadopoulos.giannhs.soft@gmail.com>
2026-03-03 15:10:46 +0200

    Add comments to House class methods and fields
    Added comments to the House class for better understanding.

--- ReferencevsObjectvsInstancevsClass/src/House.java
+++ ReferencevsObjectvsInstancevsClass/src/House.java
@@ -1,19 +1,23 @@
 
 
 public class House {
-
+    // Fields
     private String color;
 
+    // Constructor
     public House(String color) {
 
         this.color = color;
     }
 
+    // Method Getter
     public String getColor() {
         return this.color;
     }
 
+    // Method Setter
     public void setColor(String color) {
         this.color = color;
     }
+
 }
